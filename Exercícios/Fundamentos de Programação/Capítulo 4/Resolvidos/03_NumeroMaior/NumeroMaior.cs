f596918f04ee07f6020ccda3ece0eaf9a97f1d2a
Thallonys <thallonys@protonmail.ch>
2022-06-30 14:24:54 -0300

    C#

---
+++ Exercícios/Fundamentos de Programação/Capítulo 4/Resolvidos/03_NumeroMaior/NumeroMaior.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_NumeroMaior
+{
+    internal class NumeroMaior
+    {
+        private int numero1;
+
+        public int Numero1
+        {
+            get { return numero2; }
+            set
+            {
+                if (value > 0)
+                {
+                    numero1 = value;
+                }
+                else
+                {
+                    numero1 = 0;
+                }
+            }
+        }
+
+        private int numero2;
+
+        public int Numero2
+        {
+            get { return numero2; }
+            set 
+            {
+                if (value > 0)
+                { 
+                numero2 = value;
+                }
+                else
+                {
+                    numero2 = 0;
+                }
+            }
+        }
+
+
+        public void MostrarNumeroMaior()
+        {
+            if (this.Numero1 > this.Numero2)
+            {
+                Console.WriteLine($"O maior número é {this.Numero1}.");
+            }
+            else 
+            {
+                Console.WriteLine($"O maior número é {this.Numero2}.");
+            }
+        }
+
+    }
+}
---
+++ Exercícios/Fundamentos de Programação/Capítulo 4/Resolvidos/03_NumeroMaior/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_NumeroMaior
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NumeroMaior numero = new NumeroMaior();
+            int opcao = 0;
+            Console.WriteLine("Mostrar qual é o maior número entre dois números.");
+
+            do
+            {
+                Console.Write("Informe o número 1: ");
+                numero.Numero1 = int.Parse(Console.ReadLine());
+
+                Console.Write("Informe o número 2: ");
+                numero.Numero2 = int.Parse(Console.ReadLine());
+
+                numero.MostrarNumeroMaior();
+
+                Console.WriteLine("Tecle 1 para continuar ou 0 para sair.");
+                opcao = int.Parse(Console.ReadLine());
+
+                Console.Clear();
+
+            } while (opcao == 1);
+
+                Console.ReadKey();
+
+        }
+    }
+}
