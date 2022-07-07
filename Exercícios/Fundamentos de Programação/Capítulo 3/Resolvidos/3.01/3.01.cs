f4af770cd7a25154501318078b04bef17ad2e3b5
Thallonys <thallonys@protonmail.ch>
2022-07-06 23:11:25 -0300

    C#

---
+++ Exercícios/Fundamentos de Programação/Capítulo 3/Resolvidos/3.01/3.01.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            int z;
+            int w;
+            int soma;
+
+            Console.Write("Entre com o primeira inteiro: ");
+            x = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o segundo inteiro: ");
+            y = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o terceiro inteiro: ");
+            z = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o quarto inteiro: ");
+            w = int.Parse(Console.ReadLine());
+
+            soma = x + y + z + w;
+
+            Console.WriteLine($"O total da soma é {soma}.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercícios/Fundamentos de Programação/Capítulo 3/Resolvidos/3.04/3.04.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float salario;
+            double aumento;
+            double novoSalario;
+
+            Console.Write("Entre com o valor do salário: ");
+            salario = float.Parse(Console.ReadLine());
+
+            aumento = 0.25;
+            novoSalario = aumento * salario + salario;
+
+            Console.WriteLine($"O valor do novo salário é {novoSalario}.");
+            Console.ReadKey();
+        }
+    }
+}
